4d55716c85ad35584262d360a67fef9d991c9a6e
Rick Barendse <dazedeyes@yahoo.com>
2022-04-17 10:48:07 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+// set up middleware to parse JSON data
+app.uae(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
 });
