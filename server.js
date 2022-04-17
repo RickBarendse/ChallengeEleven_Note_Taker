de301f583367aa82996e59a065e1538f0131efab
Rick Barendse <dazedeyes@yahoo.com>
2022-04-17 11:59:10 -0700

    added route to get notes

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // set up middleware to parse JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
+// api routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
